a23f341137ba04a0efae0bfdcd6603074f5ee77f
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2019-04-21 06:58:48 +0000

    Run on AWS

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install fftw3 fftw3-dev
+cd T4
+nvcc *.cpp *.cu -I. -lm -lcufft -lfftw3 -std=c++14
+./a.out
+
+
